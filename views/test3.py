ed14d831961ee27abdac7a5cb53428fc6625c5d0
yejean <ileum9739@gmail.com>
2024-09-11 16:41:17 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -4,3 +4,12 @@ st.title("스트림릿 테스트 3")
 
 st.divider()  # 가로 긴 줄 구분선
 
+# password 인자 활용
+string = st.text_input(
+    'I love you',
+    placeholder='write down your favorite person's name',
+    type='password'
+)
+
+if string:
+    st.text('Your love is '+string)
