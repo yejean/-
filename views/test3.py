af9ed8e8094208407082776d23c1a3269fc0d93d
yejean <ileum9739@gmail.com>
2024-09-20 16:24:14 +0900

    Update test3.py

--- views/test3.py
+++ views/test3.py
@@ -6,5 +6,5 @@ st.divider()  # 가로 긴 줄 구분선
 
 st.image('assets/coin_head.png')
 st.image('assets/coin_tail.png')
-st.header("동전 던지기 게임에 오신 것을 환영합니다^-^")
+st.subheader("동전 던지기 게임에 오신 것을 환영합니다^-^")
 st.subheader("앞면일까요? 뒷면일까요?")
